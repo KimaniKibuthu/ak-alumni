d2aa6a958b85d8f8f927ec3a8da385f7c184c1f2
DianahOnyino <onyino.dianah@gmail.com>
2023-04-02 16:24:11 +0200

    Added className attribute

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@ import React from "react";
  
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
  
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
+import RecordList from "./pages/Record/recordList";
+import Edit from "./pages/Record/edit";
+import Create from "./pages/Record/create";
  
 const App = () => {
  return (
-   <div>
+   <div className="App">
      <Navbar />
      <Routes>
        <Route exact path="/" element={<RecordList />} />
